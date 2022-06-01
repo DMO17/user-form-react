f62342b60f3ae28169c1c64eff9a85edd3e909ba
Dahir Mohamed <dmoabdi98@gmail.com>
2022-06-01 22:41:56 +0100

    user cards complete

--- src/components/UserCards/UserCard/index.js
+++ src/components/UserCards/UserCard/index.js
@@ -1,24 +1,16 @@
 import classes from "./style.module.css";
 
-export const UserCard = () => {
+export const UserCard = ({ fullName, age, bio, imgProfile }) => {
   return (
     <div className={classes.flexContainer}>
       <div>
-        <img
-          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYeGjH5-vv2_YdwGNoZTtzZfaryVK3z3myn4Rg-mC-JcQPyLg96GnNZ7C5IAyVjm_AgEU&usqp=CAU"
-          alt="user Img"
-        />
+        <img src={imgProfile} alt="user Img" />
       </div>
       <div>
         <ul>
-          <li> Full Name: Max Scott</li>
-          <li> Age:23</li>
-          <li>
-            Bio: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat{" "}
-          </li>
+          <li> Full Name: {fullName}</li>
+          <li> Age: {age}</li>
+          <li>Bio: {bio}</li>
         </ul>
       </div>
     </div>
--- src/components/UserCards/index.js
+++ src/components/UserCards/index.js
@@ -2,13 +2,42 @@ import React from "react";
 // import { Wrapper } from "../Wrapper";
 import { UserCard } from "./UserCard";
 
-export const UserCards = ({ users }) => {
+export const UserCards = () => {
+  const users = [
+    {
+      fullName: "Scott Parker",
+      age: 22,
+      bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat",
+      imgProfile:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYeGjH5-vv2_YdwGNoZTtzZfaryVK3z3myn4Rg-mC-JcQPyLg96GnNZ7C5IAyVjm_AgEU&usqp=CAU",
+    },
+    {
+      fullName: "Scott Parker",
+      age: 22,
+      bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat",
+      imgProfile:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYeGjH5-vv2_YdwGNoZTtzZfaryVK3z3myn4Rg-mC-JcQPyLg96GnNZ7C5IAyVjm_AgEU&usqp=CAU",
+    },
+    {
+      fullName: "Scott Parker",
+      age: 22,
+      bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat",
+      imgProfile:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYeGjH5-vv2_YdwGNoZTtzZfaryVK3z3myn4Rg-mC-JcQPyLg96GnNZ7C5IAyVjm_AgEU&usqp=CAU",
+    },
+    {
+      fullName: "Scott Parker",
+      age: 22,
+      bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat",
+      imgProfile:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYeGjH5-vv2_YdwGNoZTtzZfaryVK3z3myn4Rg-mC-JcQPyLg96GnNZ7C5IAyVjm_AgEU&usqp=CAU",
+    },
+  ];
   return (
     <div>
-      <UserCard />
-      <UserCard />
-      <UserCard />
-      <UserCard />
+      {users.map((user, index) => (
+        <UserCard {...user} key={index} />
+      ))}
     </div>
   );
 };
