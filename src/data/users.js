f13e3bd6b758c4a3fb971fe84e0e757f5f08e41c
Dahir Mohamed <dmoabdi98@gmail.com>
2022-06-02 23:25:12 +0100

    sample data

--- src/data/users.js
+++ src/data/users.js
@@ -4,27 +4,27 @@ export const userList = [
     age: 22,
     bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat",
     imgProfile:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYeGjH5-vv2_YdwGNoZTtzZfaryVK3z3myn4Rg-mC-JcQPyLg96GnNZ7C5IAyVjm_AgEU&usqp=CAU",
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRsPyjYQVrpLKUJg04wMZ9NABZ1z5eYOMuzJhqJVnP38JPVi1zUnsHhg2PtRRCMvlsGgos&usqp=CAU",
   },
   {
     fullName: "Main Deffues",
     age: 45,
     bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat",
     imgProfile:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYeGjH5-vv2_YdwGNoZTtzZfaryVK3z3myn4Rg-mC-JcQPyLg96GnNZ7C5IAyVjm_AgEU&usqp=CAU",
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQsx1bqidZ9KgUxcWfOS3ZyX82HClOiZktOFK3_8tkKzMpWYBeEZD9-t14v3txxP2w6MDo&usqp=CAU",
   },
   {
     fullName: "Kevin Garnet",
     age: 15,
     bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat",
     imgProfile:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYeGjH5-vv2_YdwGNoZTtzZfaryVK3z3myn4Rg-mC-JcQPyLg96GnNZ7C5IAyVjm_AgEU&usqp=CAU",
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRkQwKzyCWJnhfsmq7LCj37_s6jI5aUGG9oyRNoNmSB9iicfflk5ot_C-_0qeCHKvbPbRM&usqp=CAU",
   },
   {
     fullName: "Lucy Becks",
     age: 44,
     bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat",
     imgProfile:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYeGjH5-vv2_YdwGNoZTtzZfaryVK3z3myn4Rg-mC-JcQPyLg96GnNZ7C5IAyVjm_AgEU&usqp=CAU",
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR1no0oIxQ_AxdC3Bjkm23Ty3-XKvxuX2BbiSG-Wjrqxbg05aw9Y3VS2327NNhf-rGNPdk&usqp=CAU",
   },
 ];
